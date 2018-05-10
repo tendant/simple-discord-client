d5c233b82c1737053e221ee9e041bb8a201c0f2d
Lei Wang <tendant@gmail.com>
2018-05-10 12:48:40 -0700

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to simple-discord-client
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/simple_discord_client/core_test.clj
@@ -0,0 +1,7 @@
+(ns simple-discord-client.core-test
+  (:require [clojure.test :refer :all]
+            [simple-discord-client.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
