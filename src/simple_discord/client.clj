8abf3e0caaa0de30affdb415f81fe9e94221e682
Lei Wang <tendant@gmail.com>
2018-05-11 15:19:31 -0700

    fix discord message function

--- src/simple_discord/client.clj
+++ src/simple_discord/client.clj
@@ -17,7 +17,7 @@
   channel-id: the id of this channel. Type snowflake
   message: {:content \"string, required, the message contents(up to 2000 characters)\"}"
   [auth channel-id message]
-  (if (clojure.string/blank? message)
+  (if (clojure.string/blank? (:content message))
     (println "discord create-message: blank message")
     (let [token-type (cond
                        (:bot auth) "Bot"
@@ -25,14 +25,15 @@
           token (or (:bot auth) (:bearer auth))
           authorization (format "%s %s" token-type token)
           uri (format "/channels/%s/messages" channel-id)
-          truncated-message (if (> (.length message) 2000)
-                              (subs message 0 2000)
-                              message)
+          content (:content message)
+          truncated-content (if (> (.length content) 2000)
+                              (subs content 0 2000)
+                              content)
           resp (http/post (endpoint uri)
                           {:headers {"Authorization" authorization}
                            :content-type :json
                            :accept :json
-                           :form-params truncated-message
+                           :form-params (assoc message :content truncated-content)
                            :throw-exceptions false})]
       (println "status:" (:status resp))
       resp)))
@@ -40,9 +41,10 @@
 (defn create-message-long
   "Send long message, split message for up to 5 separated messages"
   [auth channel-id message]
-  (let [col (re-seq #"(?s).{1,1990}" message)
-        messages (take 5 col)]
-    (doseq [msg messages]
-      (create-message auth
-                      channel-id
-                      message))))
+  (if-let [content (:content message)]
+    (let [col (re-seq #"(?s).{1,1990}" content)
+          contents (take 5 col)]
+      (doseq [msg contents]
+        (create-message auth
+                        channel-id
+                        (assoc message :content msg))))))
