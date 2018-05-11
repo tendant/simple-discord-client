3a7c07acd429c36d52b80b1c5b351fdc7683a7a3
Lei Wang <tendant@gmail.com>
2018-05-11 15:19:54 -0700

    release 0.2.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject simple-discord-client "0.2.4"
+(defproject simple-discord-client "0.2.5"
   :description "Simple Clojure discord client api, simple wrapper of discord Rest API."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
