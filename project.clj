a314512b5eb1f526df57c60780f343977ed77ccb
Lei Wang <tendant@gmail.com>
2018-05-15 14:03:25 -0700

    update to 0.2.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject simple-discord-client "0.2.5"
+(defproject simple-discord-client "0.2.6-SNAPSHOT"
   :description "Simple Clojure discord client api, simple wrapper of discord Rest API."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
